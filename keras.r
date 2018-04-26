c20a7c6fefa48c0ada1f582dc27dad0ed45ee95d
daanvandermaas <daanvandermaas@hotmail.com>
2018-04-26 10:01:37 +0200

    x

--- keras.r
+++ keras.r
@@ -20,6 +20,7 @@ test = list.files('db/landuse/land-valid')[1:20]
 
 
 ######Parameters
+epochs = 280
 batch_size = 4
 parts = 4
   h = as.integer(608) #heigth image dim image = 2448
@@ -34,7 +35,7 @@ pick = 5
 
 source('big.r')
 
-opt<-optimizer_adam( lr= 0.001 , decay = 0,  clipnorm = 1 )
+opt<-optimizer_adam( lr= 2.7^(-4) , decay = 0,  clipnorm = 1 )
 
 compile(model, loss="categorical_crossentropy", optimizer=opt, metrics = "accuracy")
 
@@ -42,25 +43,30 @@ sess = k_get_session()
 
 
 
+
+
 #Train the network
-for (i in 1:2000000) {
-  print(i)
-  #lees 50 random plaatjes in
-  samp = sample(c(1:nrow(train)), 1)
+for (epoch in 1:epochs) {
+  order = sample(c(1:nrow(train) ), nrow(train))
+  train = train[order,]
+  
+  print(paste('epoch:', epoch))
+  
+  
+  for(i in 1:nrow(train)){
+  
   windows = c(1:parts^2)
   
-  batch_files = read_batch(files = train$images[samp], format = 'jpg', channels = channels, windows = windows, parts=parts, w= w, h=h)
+  batch_files = read_batch(files = train$images[i], format = 'jpg', channels = channels, windows = windows, parts=parts, w= w, h=h)
   batch_labels =  read_labels_select(files = train$labels[samp], windows = windows, class = class, parts=parts, w=w, h=h, pick = pick)
 
   
   model$fit( x= batch_files, y= batch_labels, batch_size = batch_size, epochs = 1L, class_weight = list(1,1)  )
   
-  if(i%%4000 == 0 ){
-    print(i)
-    print('model saved')
-    model$save( paste0('db/model_', i) )
+}
+    model$save( paste0('db/model_', epoch) )
     
-  }
+  
   
   
 }
